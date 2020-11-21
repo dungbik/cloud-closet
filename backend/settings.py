28cd4285be5cb3adae68ecda2a67c8324069be3e
dungbik <q1w2e30630@gmail.com>
2020-11-21 18:28:19 +0900

    allowed host 추가

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5@*tbfzqk03s^dczq&3i2!n&_)w1)bv3yf1)=#d=j2n@^j+)wj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['catcosmic.pythonanywhere.com'] 
 
 
 # Application definition
