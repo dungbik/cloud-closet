71ddf1afa5c53a3fc25608af72469020bcac06bb
dungbik <q1w2e30630@gmail.com>
2020-11-21 18:25:22 +0900

    실수 수정

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
